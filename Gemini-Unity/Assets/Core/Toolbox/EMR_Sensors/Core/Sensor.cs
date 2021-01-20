d9480bbd7e67f971d0f7a20f6275e4f48a766a0f
Skarsh <skarshaug.thomas@gmail.com>
2021-01-20 14:38:57 +0100

    Fix missing Visualizers in Trondheim scene.

--- Gemini-Unity/Assets/Core/Toolbox/EMR_Sensors/Core/Sensor.cs
+++ Gemini-Unity/Assets/Core/Toolbox/EMR_Sensors/Core/Sensor.cs
@@ -63,7 +63,7 @@ namespace Gemini.EMRS.Core
                 {
                     if (gate)
                     {
-                        SendMessage();
+                        //SendMessage();
                         nextActionTime = nextOSPtime + 1/(double)SensorUpdateHz;
                     }
                 }
