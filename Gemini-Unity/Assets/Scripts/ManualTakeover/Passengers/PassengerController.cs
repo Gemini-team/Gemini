def714881b9c66ce9c96119df7048aa6ecf71f5b
Mikael R Hanssen <mikaelrh@stud.ntnu.no>
2021-03-02 14:59:45 +0100

    Implement PassengerQueue
    - Moves passenger to waiting area, and assigns position in queue

--- Gemini-Unity/Assets/Scripts/ManualTakeover/Passengers/PassengerController.cs
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/Passengers/PassengerController.cs
@@ -6,12 +6,16 @@ using UnityEngine;
 /// Spawns passengers. Controls passenger behaviours (idle and embark/disembark)
 /// Add as component to waiting area.
 /// </summary>
-public class PassengerController : MonoBehaviour {
+public class PassengerController : MonoBehaviour
+{
     private List<Passenger> passengers = new List<Passenger>();
     private BoatController ferry;
 
     public int spawnAmount;
     public GameObject passengerTemplate;
+    public PassengerQueue queue;
+
+    public bool embark;
 
     /// <summary>
     /// Generates a random destination on the attached nav mesh
@@ -19,21 +23,35 @@ public class PassengerController : MonoBehaviour {
     /// <returns>Vector3</returns>
     private Vector3 IdleDestination() => transform.position + new Vector3(Random.Range(-0.5f, 0.5f) * transform.localScale.x, passengerTemplate.transform.localScale.y, Random.Range(-0.5f, 0.5f) * transform.localScale.z);
 
-    private void Start() {
+    private void Start()
+    {
         ferry = GameObject.FindGameObjectWithTag("Player").GetComponent<BoatController>();
 
-        for (int i = 0; i < spawnAmount; i++) {
+        for (int i = 0; i < spawnAmount; i++)
+        {
             GameObject instance = Instantiate(
-                original: passengerTemplate,
+                original: passengerTemplate, 
                 position: IdleDestination(),
                 rotation: Quaternion.identity);
             passengers.Add(instance.GetComponent<Passenger>());
         }
     }
 
-    void Update() {
-        foreach (Passenger passenger in passengers) {
-            if (!passenger.IsBusy) {
+    void Update()
+    {
+        if (embark) {
+            embark = false;
+
+            Passenger passenger = passengers[0];
+            passenger.inTransit = true;
+            queue.Enqueue(passenger);
+            passengers.RemoveAt(0);
+        }
+
+        foreach (Passenger passenger in passengers)
+        {
+            if (!passenger.IsBusy)
+            {
                 passenger.SetDestination(IdleDestination(), Random.Range(15, 30));
             }
         }
---
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/Passengers/PassengerQueue.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PassengerQueue : MonoBehaviour {
+    private const float SPACING = 1.5f;
+
+    private List<Passenger> passengers = new List<Passenger>();
+
+    public void Enqueue(Passenger passenger) {
+        passengers.Add(passenger);
+        passenger.SetDestination(transform.position + transform.forward * SPACING * passengers.Count);
+    }
+
+    void OnDrawGizmos() {
+        Gizmos.color = Color.cyan;
+        Gizmos.DrawSphere(transform.position, 0.5f);
+        Gizmos.DrawLine(transform.position, transform.position + transform.forward * 4);
+    }
+}
