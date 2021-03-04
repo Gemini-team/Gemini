97d66c51dae05f37aa56e9761aadafcaef1ce9d5
Thomas Skarshaug <thomas.skarshaug@zeabuz.com>
2021-03-04 14:12:40 +0100

    Move ocean and clouds into Environment folder and add namespaces to them

---
+++ Gemini-Unity/Assets/Scripts/Environment/Ocean/Water/Spawner.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+namespace Gemini.Environment.Ocean
+{
+    public class Spawner : MonoBehaviour
+    {
+        public GameObject WaterPrefab;
+        public int size;
+        // Start is called before the first frame update
+        void Start()
+        {
+            Vector3 parentPosition = transform.position;
+            for (int i = 0; i < size; i++)
+            {
+                for (int j = 0; j < size; j++)
+                {
+                    Instantiate(WaterPrefab, parentPosition + new Vector3(5 * i, 0, 5 * j), Quaternion.identity);
+                }
+            }
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+    }
+}
