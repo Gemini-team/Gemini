8ebaf268f7082c6707252cfd5394a363b09e0623
Thomas Skarshaug <thomas.skarshaug@zeabuz.com>
2021-03-04 14:10:45 +0100

    Move CircularMovement class into Gemini.Misc namespace

--- Gemini-Unity/Assets/Scripts/Misc/CircularMovement.cs
+++ Gemini-Unity/Assets/Scripts/Misc/CircularMovement.cs
@@ -1,24 +1,24 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
-public class CircularMovement : MonoBehaviour
+namespace Gemini.Misc
 {
-
-    private Vector3 startPos;
-    public float speed = 0.05f;
-    public float radius = 2;
-
-
-    // Start is called before the first frame update
-    void Start()
+    public class CircularMovement : MonoBehaviour
     {
-        startPos = transform.position;
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        transform.position = startPos + radius * new Vector3(Mathf.Cos(speed * Time.time), 0, Mathf.Sin(speed * Time.time));
+        private Vector3 startPos;
+        public float speed = 0.05f;
+        public float radius = 2;
+
+        // Start is called before the first frame update
+        void Start()
+        {
+            startPos = transform.position;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            transform.position = startPos + radius * new Vector3(Mathf.Cos(speed * Time.time), 0, Mathf.Sin(speed * Time.time));
+        }
     }
 }
