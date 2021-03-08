2440ea15845f78937ab63e80233b38b13c0e836e
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2021-03-08 19:41:59 +0100

    Add if statement to check whether to prevent ArgumentOutOfRangeException

--- Gemini-Unity/Assets/Scripts/Networking/Services/Simulation/SimulationServiceImpl.cs
+++ Gemini-Unity/Assets/Scripts/Networking/Services/Simulation/SimulationServiceImpl.cs
@@ -39,21 +39,17 @@ namespace Gemini.Networking.Services {
 
             ThreadManager.ExecuteOnMainThread(() =>
             {
-                try
+                for (int boatIdx = 0; boatIdx < _boats.Length; boatIdx++)
                 {
-                    for (int boatIdx = 0; boatIdx < _boats.Length; boatIdx++)
+                    if (boatIdx < _boats.Length)
                     {
                         _boats[boatIdx].transform.position = new Vector3(poses[boatIdx].East,0,poses[boatIdx].North);
                         float Heading = poses[boatIdx].Heading;
                         Quaternion QuaternionRot = Quaternion.AngleAxis(Heading, new Vector3(0, 1, 0));
                         _boats[boatIdx].transform.rotation = QuaternionRot;
                     }
-                    signalEvent.Set();
                 }
-                catch(IndexOutOfRangeException exception)
-                {
-                    signalEvent.Set();
-                }
+                signalEvent.Set();
             });
 
             // Wait for the event to be triggered from the action, signaling that the action is finished
@@ -61,11 +57,6 @@ namespace Gemini.Networking.Services {
             // Unity main thread.
             signalEvent.WaitOne();
             signalEvent.Close();
-
-                
-            // Sets the step size in simulation controller
-            //_simulationController.SetStepSize(request.StepSize);
-
 
             return await Task.FromResult(new StepResponse
             {
