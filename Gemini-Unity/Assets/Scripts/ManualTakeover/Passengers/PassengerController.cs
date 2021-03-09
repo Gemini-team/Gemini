8bda496732e45e493f3fdb42699dcab55801b84d
Mikael R Hanssen <mikaelrh@stud.ntnu.no>
2021-03-09 17:15:01 +0100

    Merge PassagerManager into PassengerController. In PassengerController, add support for multiple spawn areas

--- Gemini-Unity/Assets/Scripts/ManualTakeover/Passengers/PassengerController.cs
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/Passengers/PassengerController.cs
@@ -2,30 +2,33 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-/// <summary>
-/// Spawns passengers. Controls passenger behaviours (idle and embark/disembark)
-/// Add as component to waiting area.
-/// </summary>
 public class PassengerController : MonoBehaviour {
-    private List<Passenger> passengers = new List<Passenger>();
-
     public int spawnAmount;
     public GameObject passengerTemplate;
     public PassengerQueue queue;
+    public Transform[] spawnAreas;
 
-    public bool embark;
+    private List<Passenger> passengers = new List<Passenger>();
 
     /// <summary>
     /// Generates a random destination on the attached nav mesh
     /// </summary>
     /// <returns>Vector3</returns>
-    private Vector3 IdleDestination() => transform.position + new Vector3(Random.Range(-0.5f, 0.5f) * transform.localScale.x, passengerTemplate.transform.localScale.y, Random.Range(-0.5f, 0.5f) * transform.localScale.z);
+    private Vector3 RandomDestination() {
+        Transform area = spawnAreas[Random.Range(0, spawnAreas.Length)];
+        return area.position + new Vector3(
+            Random.Range(-0.5f, 0.5f) * area.localScale.x,
+            passengerTemplate.transform.localScale.y,
+            Random.Range(-0.5f, 0.5f) * area.localScale.z);
+    }
 
     private void Start() {
+        queue = GetComponentInChildren<PassengerQueue>();
+
         for (int i = 0; i < spawnAmount; i++) {
             GameObject instance = Instantiate(
                 original: passengerTemplate,
-                position: IdleDestination(),
+                position: RandomDestination(),
                 rotation: Quaternion.identity);
             passengers.Add(instance.GetComponent<Passenger>());
         }
@@ -34,7 +37,7 @@ public class PassengerController : MonoBehaviour {
     void Update() {
         foreach (Passenger passenger in passengers) {
             if (!passenger.IsBusy) {
-                passenger.SetDestination(IdleDestination(), Random.Range(15, 30));
+                passenger.SetDestination(RandomDestination(), Random.Range(15, 30));
             }
         }
     }
