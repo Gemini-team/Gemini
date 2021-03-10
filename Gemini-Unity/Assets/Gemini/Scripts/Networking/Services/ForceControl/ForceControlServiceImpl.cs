dcf5a5095048a7664442bbe79a5892e02d1de862
Thomas Skarshaug <skarshaug.thomas@gmail.com>
2021-03-10 20:35:59 +0100

    Implementing the ForceControl service

---
+++ Gemini-Unity/Assets/Gemini/Scripts/Networking/Services/ForceControl/ForceControlServiceImpl.cs
@@ -0,0 +1,68 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Gemini.Forcecontrol;
+using Grpc.Core;
+using System.Threading.Tasks;
+using Gemini.Core;
+using System.Threading;
+
+namespace Gemini.Networking.Services
+{
+    public class ForceControlServiceImpl : ForceControl.ForceControlBase
+    {
+
+        private ForceController[] _forceControllers;
+
+        private Vector3 _force = new Vector3();
+        private Vector3 _torque = new Vector3();
+
+        public ForceControlServiceImpl(ForceController[] forceControllers)
+        {
+            this._forceControllers = forceControllers; 
+        }
+
+        public override async Task<ForceResponse> ApplyForce(ForceRequest request, ServerCallContext context)
+        {
+
+            _force.x = request.GeneralizedForce.X;
+            _force.y = request.GeneralizedForce.Y;
+            _force.z = request.GeneralizedForce.Z;
+
+            _torque.x = request.GeneralizedForce.K;
+            _torque.y = request.GeneralizedForce.M;
+            _torque.z = request.GeneralizedForce.N;
+
+            // Create the event that triggers when the execution of the action is finished.
+            ManualResetEvent signalEvent = new ManualResetEvent(false);
+
+            ThreadManager.ExecuteOnMainThread(() =>
+            {
+                foreach (ForceController forceController in _forceControllers)
+                {
+                    if (forceController.name == request.VesselId)
+                    {
+                        forceController.Force = _force;
+                        forceController.Torque = _torque;
+                    }
+                }
+
+                signalEvent.Set();
+            });
+
+            // Wait for the event to be triggered from the action, signaling that the action is finished
+            // This is required becaue we are reading and depending on state from a resource running on the
+            // Unity main thread.
+            signalEvent.WaitOne();
+            signalEvent.Close();
+
+
+            return await Task.FromResult(new ForceResponse
+            {
+                Success = true,
+            });
+
+        }
+    }
+}
+
---
+++ Gemini-Unity/Assets/Gemini/Scripts/Networking/Services/ForceControl/ForceController.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+
+namespace Gemini.Networking.Services {
+
+    [RequireComponent(typeof(Rigidbody))]
+    public class ForceController : MonoBehaviour
+    {
+
+        private Vector3 _force;
+        private Vector3 _torque;
+
+        public Vector3 Force
+        {
+            set => _force = value;
+            get => _force;
+        }
+
+        public Vector3 Torque
+        {
+            set => _torque = value;
+            get => _torque;
+        }
+
+        void FixedUpdate()
+        {
+            gameObject.GetComponent<Rigidbody>().AddForce(_force);
+            gameObject.GetComponent<Rigidbody>().AddTorque(_torque);
+        }
+    }
+
+}
