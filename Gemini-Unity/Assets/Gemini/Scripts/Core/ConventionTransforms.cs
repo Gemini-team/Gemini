1538bd2f3081018ae2a14ff72ca7075cb6b0612e
Thomas Skarshaug <thomas.skarshaug@zeabuz.com>
2021-03-17 13:52:15 +0100

    Restructuring, Navigation fix and FFT-Ocean (#35)
    * started instanceIndirect point cloud visualizer that will eventually replace the old point cloud
    
    * Added content.meta to gitignore
    
    * restructured all content under a unified Gemini folder
    
    * Deleted scenes
    
    * Added metafiles
    
    * revert to a better merging point to develop
    
    * added metafiles again...
    
    * created a main folder for all files
    
    * Moved scenes into common folder
    
    * Move Plugins folder
    
    * Untoggle Fog
    
    * Toggle off fog.
    
    * started creating example scene, and restructured folder contents
    
    * Added FFT-ocean, but the ocean shader must be rewritten to support HDRP
    
    * Added readme for FFT-ocean to give credits to the original developer
    
    * Add .vsconfig to .gitignore, change IP-address for vm, flip y axis
    
    * Delete unused imports
    
    * Found a way to get compute shader results from FFT ocean into shader graph using textures. Started with creating a HDRP shader for the ocean
    
    * Add FFT-Ocean License
    
    * Fix title
    
    * Made changes to the ocean shader to take into account the various cascades of the ocean surface. The ocean meshes do not align properly
    
    * Add functions for Converting Orientation based on Euler angles
    
    * Send QuaternionOrientation based on EulerAngles which gets converted to NED
    
    * Flip coordinate axis of LidarData
    
    * Update .gitignore, Generate .meta files for Plugins folder and FFT-Ocean LICENSE
    
    Co-authored-by: kjetv <kjetv@stud.ntnu.no>

---
+++ Gemini-Unity/Assets/Gemini/Scripts/Core/ConventionTransforms.cs
@@ -0,0 +1,113 @@
+﻿
+using UnityEngine;
+
+namespace Gemini.Core
+{
+    public static class ConventionTransforms
+    {
+        private static Vector3 TranslationUnityToNED(Vector3 vec)
+        {
+            return new Vector3(vec.z, vec.x, -vec.y);
+        }
+
+        private static Vector3 TranslationNEDToUnity(Vector3 vec)
+        {
+            return new Vector3(vec.y, -vec.z, vec.x);
+        }
+
+        private static Vector3 RotationUnityToNED(Vector3 angleVec)
+        {
+            return new Vector3(-angleVec.z, -angleVec.x, angleVec.y);
+        }
+
+        private static Vector3 RotationNEDToUnity(Vector3 angleVec)
+        {
+            return new Vector3(-angleVec.y, angleVec.z, -angleVec.x);
+        }
+
+        private static Quaternion QuaternionNEDToUnity(Quaternion quaternionVec)
+        {
+            return new Quaternion(
+                quaternionVec.y,
+                quaternionVec.w,
+                quaternionVec.z,
+                quaternionVec.x);
+        }
+
+        private static Quaternion QuaternionUnityToNED(Quaternion quaternionVec)
+        {
+            return Quaternion.Inverse(QuaternionNEDToUnity(quaternionVec));
+
+        }
+
+        public static Vector3 ForceUnityToNED(Vector3 force)
+        {
+            return TranslationUnityToNED(force);
+        }
+
+        public static Vector3 ForceNEDToUnity(Vector3 force)
+        {
+            return TranslationNEDToUnity(force);
+        }
+
+        public static Vector3 TorqueUnityToNED(Vector3 torque)
+        {
+            return RotationUnityToNED(torque);
+        }
+
+        public static Vector3 TorqueNEDToUnity(Vector3 torque)
+        {
+            return RotationNEDToUnity(torque);
+        }
+
+        public static Vector3 PositionUnityToNED(Vector3 position)
+        {
+            return TranslationUnityToNED(position);
+        }
+
+        public static Vector3 PositionNEDToUnity(Vector3 position)
+        {
+            return TranslationNEDToUnity(position);
+        }
+
+        public static Vector3 VelocityUnityToNED(Vector3 velocity)
+        {
+            return TranslationUnityToNED(velocity);
+        }
+
+        public static Vector3 VelocityNEDToUnity(Vector3 velocity)
+        {
+            return TranslationNEDToUnity(velocity);
+        }
+
+        public static Vector3 AngularVelocityUnityToNED(Vector3 angularVelocity)
+        {
+            return RotationUnityToNED(angularVelocity);
+        }
+
+        public static Vector3 AngularVelocityNEDToUnity(Vector3 angularVelocity)
+        {
+            return RotationNEDToUnity(angularVelocity);
+        }
+
+        public static Vector3 EulerOrientationUnityToNED(Vector3 orientation)
+        {
+            return RotationUnityToNED(orientation);
+        }
+
+        public static Vector3 EulerOrientationNEDToUnity(Vector3 orientation)
+        {
+            return RotationNEDToUnity(orientation);
+        }
+        
+        public static Quaternion QuaternionOrientationUnityToNED(Quaternion orientation)
+        {
+            return QuaternionUnityToNED(orientation);
+        }
+
+        public static Quaternion QuaternionOrientationNEDToUnity(Quaternion orientation)
+        {
+            return QuaternionNEDToUnity(orientation);
+        }
+    }
+}
