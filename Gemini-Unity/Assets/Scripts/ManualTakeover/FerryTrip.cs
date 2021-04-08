963bcb97d952061af5dbd8495bf3a49d82e22161
Mikael R Hanssen <mikaelrh@stud.ntnu.no>
2021-04-08 15:56:14 +0200

    Implement CameraController
    - Replaces CameraManager
    - Control free camera with the mouse (right click to move forwards, left click to move backwards)
    - Define predetermined mounts and toggle between them with the number keys
    - Dismount with the middle mouse button

--- Gemini-Unity/Assets/Scripts/ManualTakeover/FerryTrip.cs
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/FerryTrip.cs
@@ -3,19 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FerryTrip : AnimatedTrip {
-    private Camera mountedCamera;
     public FerryController ferry { get; private set; }
 
     protected override void Start() {
         base.Start();
         ferry = GetComponent<FerryController>();
-        mountedCamera = GetComponentInChildren<Camera>();
         ferry.TryConnectToDock();
     }
 
     public override void SkipToEnd() {
         reverse = !reverse;
-        mountedCamera.transform.localRotation = Quaternion.Euler(0, reverse ? 180 : 0, 0);
         base.SkipToEnd();
     }
 
@@ -27,6 +24,5 @@ public class FerryTrip : AnimatedTrip {
     public override void Play() {
         if (!ferry.TryDisconnectFromDock()) return;
         base.Play();
-        
     }
 }
---
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/Managers/CameraController.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour {
+    private const float LERP_SPEED = 3f;
+
+    public Transform[] mounts;
+    public float speed, mouseSensitivity;
+
+    private Transform mount;
+
+    private void Update() {
+        for (int i = 0; i < mounts.Length; i++) {
+            if (Input.GetKeyDown(KeyCode.Alpha1 + i)) {
+                mount = mounts[i];
+                break;
+            }
+        }
+
+        if (Input.GetMouseButtonDown(2)) {
+            mount = null;
+        }
+
+        if (mount == null) {
+            Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            transform.rotation = Quaternion.Euler(Mathf.Clamp(transform.eulerAngles.x - input.y, -89.9f, 89.8f), transform.eulerAngles.y + input.x, 0);
+
+            bool leftClick = Input.GetMouseButton(0), rightClick = Input.GetMouseButton(1);
+            if (leftClick || rightClick) {
+                transform.position += transform.forward * speed * Time.deltaTime * (leftClick ? -1 : 1);
+            }
+        } else {
+            transform.position = Vector3.Lerp(transform.position, mount.position, LERP_SPEED * Time.deltaTime);
+            transform.rotation = Quaternion.Lerp(transform.rotation, mount.rotation, LERP_SPEED * Time.deltaTime);
+        }
+    }
+}
