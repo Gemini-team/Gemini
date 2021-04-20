757a9409a2eb6de3bc8f4dd92073a3ab27713c16
Mikael R Hanssen <mikaelrh@stud.ntnu.no>
2021-04-20 14:36:42 +0200

    Implement camera zoom (uses scrollwheel)

--- Gemini-Unity/Assets/Scripts/ManualTakeover/Managers/CameraController.cs
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/Managers/CameraController.cs
@@ -3,10 +3,16 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class CameraController : MonoBehaviour {
-    private const float TRANSITION_DURATION = 1;
+    private const float 
+        MOUSE_SENSITIVITY = 1,
+        SCROLL_SENSITIVITY = 20,
+        TRANSITION_DURATION = 1, 
+        MIN_FOV = 30;
 
     public Transform[] mounts;
-    public float speed, mouseSensitivity;
+    public float speed;
+    private float maxFOV;
+    private Camera cam;
 
     private int? mountI = null;
     private Transform Mount => mountI.HasValue ? mounts[mountI.Value] : null;
@@ -19,6 +25,9 @@ public class CameraController : MonoBehaviour {
 
     private void Start() {
         lookRotation = transform.eulerAngles;
+        cam = GetComponent<Camera>();
+        maxFOV = cam.fieldOfView;
+
         if (mounts.Length > 0) {
             MountTo(0);
             transition = 1;  // Instantly move to mount
@@ -41,11 +50,12 @@ public class CameraController : MonoBehaviour {
 
         if (Input.GetMouseButtonDown(2)) {
             lookRotation = transform.eulerAngles;
+            cam.fieldOfView = maxFOV;
             mountI = null;
         }
 
         if (!mountI.HasValue) {
-            Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y"));
+            Vector2 input = new Vector2(Input.GetAxis("Mouse X"), Input.GetAxis("Mouse Y")) * MOUSE_SENSITIVITY;
             // Update euler angles as a vector to avoid quaternion clamping issues
             lookRotation += new Vector3(-input.y, input.x);
             lookRotation.x = Mathf.Clamp(lookRotation.x, -89.9f, 89.9f);
@@ -57,6 +67,11 @@ public class CameraController : MonoBehaviour {
             }
         }
         else {
+            float scroll = Input.GetAxisRaw("Mouse ScrollWheel");
+            if (scroll != 0) {
+                cam.fieldOfView = Mathf.Clamp(cam.fieldOfView - scroll * SCROLL_SENSITIVITY, MIN_FOV, maxFOV);
+            }
+
             transition = Mathf.Clamp01(transition + Time.deltaTime / TRANSITION_DURATION);
             transform.position = Vector3.Lerp(startPos, Mount.position, transition);
             transform.rotation = Quaternion.Lerp(startRot, Mount.rotation, transition);
@@ -68,5 +83,7 @@ public class CameraController : MonoBehaviour {
         startRot = transform.rotation;
         transition = 0;
         mountI = index;
+
+        cam.fieldOfView = maxFOV;
     }
 }
