0b19af8390a71a59aceb8be6cf2eb62d54229e85
Mikael R Hanssen <mikaelrh@stud.ntnu.no>
2021-04-20 16:00:34 +0200

    Implement screenshot hotkey (F2). Saved at relative path './screenshots'

---
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/TakeScreenshot.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeScreenshot : MonoBehaviour {
+    private const string ROOT = "./screenshots/";
+
+    public void Start() {
+        if (!System.IO.Directory.Exists(ROOT)) {
+            System.IO.Directory.CreateDirectory(ROOT);
+        }
+    }
+
+    public void Update() {
+        if (Input.GetKeyDown(KeyCode.F2)) {
+            string path = System.IO.Path.GetFullPath(ROOT + System.DateTime.Now + ".png");
+            ScreenCapture.CaptureScreenshot(path);
+            Debug.Log("Screenshot saved at " + path);
+        }
+    }
+}
