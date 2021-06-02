25636c52227b9520435e82a7df10e2f44818394e
mikael-rh <hanssen.mikael@gmail.com>
2021-06-02 14:07:44 +0200

    Implement PassengerAudio
    - Footsteps where volume varies based on relative speed

---
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/Audio/PassengerAudio.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class PassengerAudio : AudioManager {
+	public AudioClip footsteps;
+
+	private AudioSource channel;
+	private NavMeshAgent agent;
+	private Vector3 prevPos;
+
+	protected override void AudioSetup() {
+		prevPos = transform.localPosition;
+		agent = GetComponent<NavMeshAgent>();
+		channel = PlayInfinite(footsteps);
+	}
+
+	private void Update() {
+		float speed = (transform.localPosition - prevPos).magnitude / Time.deltaTime;
+		channel.volume = speed / agent.speed;
+	}
+
+	private void LateUpdate() {
+		prevPos = transform.localPosition;
+	}
+}
