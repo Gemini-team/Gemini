a22549f5cce2ce28db43cd3f8fce380fe28d6d4c
mikael-rh <hanssen.mikael@gmail.com>
2021-06-03 10:28:58 +0200

    Implement placeholder smoke effect on manual takeover
    - Uses HDRP's particle system shader samples

---
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/FerryParticleFX.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FerryParticleFX : MonoBehaviour {
+	private ParticleSystem smoke;
+
+	private void Start() {
+		smoke = transform.Find("Smoke").GetComponent<ParticleSystem>();
+		FindObjectOfType<Scenario>().OnManualTakeover.AddListener(smoke.Play);
+	}
+}
