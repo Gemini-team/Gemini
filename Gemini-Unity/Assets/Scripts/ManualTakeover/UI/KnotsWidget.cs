9ffc2fcd94d5f9b00cb95c94a9a9d731fe86dc22
mikael-rh <hanssen.mikael@gmail.com>
2021-06-10 00:33:34 +0200

    Limit KnotsWidget refresh rate

--- Gemini-Unity/Assets/Scripts/ManualTakeover/UI/KnotsWidget.cs
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/UI/KnotsWidget.cs
@@ -4,18 +4,21 @@ using UnityEngine;
 using UnityEngine.UI;
 
 public class KnotsWidget : MonoBehaviour {
-    private const float MPS_TO_KNOTS = 1.94384449f;
-    private Text text;
-    private FerryController ferry;
+	private const float MPS_TO_KNOTS = 1.94384449f, UPDATE_INTERVAL = 0.5f;
+	private Text text;
+	private FerryController ferry;
 
-    // Start is called before the first frame update
-    private void Start() {
-        ferry = GameObject.FindGameObjectWithTag("Player").GetComponent<FerryController>();
-        text = GetComponent<Text>();
-    }
+	private void Start() {
+		ferry = GameObject.FindGameObjectWithTag("Player").GetComponent<FerryController>();
+		text = GetComponent<Text>();
 
-    // Update is called once per frame
-    private void Update() {
-        text.text = System.Math.Round(ferry.Speed * MPS_TO_KNOTS, 1) + " kn";
-    }
+		StartCoroutine(Draw());
+	}
+
+	private IEnumerator Draw() {
+		while (true) {
+			text.text = System.Math.Round(ferry.Speed * MPS_TO_KNOTS, 1) + " kn";
+			yield return new WaitForSeconds(UPDATE_INTERVAL);
+		}
+	}
 }
