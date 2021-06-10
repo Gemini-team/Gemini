dbaec86564496670eb672c80d37f4c8182144401
Mikael R Hanssen <mikaelrh@stud.ntnu.no>
2021-06-10 17:54:29 +0200

    Implement script for drawing a bounding box around objects in the scene
    - Drawn in screen space (UI)

---
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/UI/ObjectDetectionWidget.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectDetectionWidget : MonoBehaviour {
+    public const float SIZE_MULTIPLIER = 1.5f;
+
+    public Renderer target;
+    private Camera cam;
+
+    private RectTransform rt;
+
+    private void Start() {
+        cam = Camera.main;
+        rt = GetComponent<RectTransform>();
+    }
+
+    private void Update() {
+        Vector2 min = cam.WorldToScreenPoint(target.bounds.min);
+        Vector2 max = cam.WorldToScreenPoint(target.bounds.max);
+
+        rt.localPosition = (min + max) * 0.5f;
+        rt.sizeDelta = (max - min) * SIZE_MULTIPLIER;
+    }
+}
