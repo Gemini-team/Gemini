c9370d57138d82e0e06c1e7904bda84292333673
Mikael R Hanssen <mikaelrh@stud.ntnu.no>
2021-06-11 15:59:36 +0200

    Don't draw object detection bounding box if object is behind camera plane

--- Gemini-Unity/Assets/Scripts/ManualTakeover/UI/ObjectDetectionWidget.cs
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/UI/ObjectDetectionWidget.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ObjectDetectionWidget : MonoBehaviour {
     public const float SIZE_MULTIPLIER = 1.5f;
@@ -8,6 +9,7 @@ public class ObjectDetectionWidget : MonoBehaviour {
     public Renderer target;
     private Camera cam;
 
+    private Image image;
     private RectTransform rt;
 
     private void Start() {
@@ -18,9 +20,17 @@ public class ObjectDetectionWidget : MonoBehaviour {
 
         cam = Camera.main;
         rt = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
     }
 
     private void Update() {
+        Vector2 dir = target.transform.position - cam.transform.position;
+        if (Vector3.Dot(dir, cam.transform.forward) < 0) {
+            image.enabled = false;
+            return;  // Don't draw bounding box if object is behind camera
+        }
+        image.enabled = true;
+
         Vector2 min = cam.WorldToScreenPoint(target.bounds.min);
         Vector2 max = cam.WorldToScreenPoint(target.bounds.max);
 
