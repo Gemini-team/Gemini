c75b9a801b6f138867e9f5b619dd4c439d402a64
Thomas Skarshaug <thomas.skarshaug@zeabuz.com>
2021-07-01 19:15:41 +0200

    Develop (#48)
    * Delete old force protobuf file
    
    * Rename remotecontrol protobuf and gRPC definition to force_control
    
    * Fix path to Protobuf files in Gemini after folder restructuring
    
    * Add generated protobuf and gRPC files for forcecontrol service
    
    * Remove unnecessary docstring
    
    * Implementing the ForceControl service
    
    * Change server IP
    
    * Add *.code-workspace to .gitignore
    
    * Converting force and torque to Unity coord system and adding force and torque relatively
    
    * Send velocity and angularvelocity data if the gameobject has a rigibody component
    
    * Use wall time instead of Scenario time to prevent jumping back in time
    
    * Update sensor time with time from OSP
    
    * Add IP for second VM
    
    * Update navigation message difintion to include vesselname
    
    * Update navigation protofile to include vesselName field
    
    * Add Vesselname field and send it
    
    * Fixing piren frame offset
    
    * Remove conflict markers
    
    * Change ServerIP
    
    * Simple Time fixes. (#4)
    
    * Use wall time instead of Scenario time to prevent jumping back in time
    
    * Update sensor time with time from OSP
    
    * Add IP for second VM
    
    * Update navigation message difintion to include vesselname
    
    * Update navigation protofile to include vesselName field
    
    * Add Vesselname field and send it
    
    * Fixing piren frame offset
    
    * Change ServerIP
    
    * Force Controller etc. (#5)
    
    * Use wall time instead of Scenario time to prevent jumping back in time
    
    * Update sensor time with time from OSP
    
    * Add IP for second VM
    
    * Update navigation message difintion to include vesselname
    
    * Update navigation protofile to include vesselName field
    
    * Add Vesselname field and send it
    
    * Fixing piren frame offset
    
    * Change ServerIP
    
    * ForceController etc (#6)
    
    * Delete old force protobuf file
    
    * Rename remotecontrol protobuf and gRPC definition to force_control
    
    * Fix path to Protobuf files in Gemini after folder restructuring
    
    * Add generated protobuf and gRPC files for forcecontrol service
    
    * Remove unnecessary docstring
    
    * Implementing the ForceControl service
    
    * Change server IP
    
    * Add *.code-workspace to .gitignore
    
    * Converting force and torque to Unity coord system and adding force and torque relatively
    
    * Send velocity and angularvelocity data if the gameobject has a rigibody component
    
    * Use wall time instead of Scenario time to prevent jumping back in time
    
    * Update sensor time with time from OSP
    
    * Add IP for second VM
    
    * Update navigation message difintion to include vesselname
    
    * Update navigation protofile to include vesselName field
    
    * Add Vesselname field and send it
    
    * Fixing piren frame offset
    
    * Remove conflict markers
    
    * Change ServerIP
    
    * Merge Develop (#7)
    
    * Delete old force protobuf file
    
    * Rename remotecontrol protobuf and gRPC definition to force_control
    
    * Fix path to Protobuf files in Gemini after folder restructuring
    
    * Add generated protobuf and gRPC files for forcecontrol service
    
    * Remove unnecessary docstring
    
    * Implementing the ForceControl service
    
    * Change server IP
    
    * Add *.code-workspace to .gitignore
    
    * Converting force and torque to Unity coord system and adding force and torque relatively
    
    * Send velocity and angularvelocity data if the gameobject has a rigibody component
    
    * Use wall time instead of Scenario time to prevent jumping back in time
    
    * Update sensor time with time from OSP
    
    * Add IP for second VM
    
    * Update navigation message difintion to include vesselname
    
    * Update navigation protofile to include vesselName field
    
    * Add Vesselname field and send it
    
    * Fixing piren frame offset
    
    * Remove conflict markers
    
    * Change ServerIP
    
    * Change IP to localhost for docker container (#8)

---
+++ Gemini-Unity/Assets/Gemini/Scripts/Networking/Services/ForceControl/ForceController.cs
@@ -0,0 +1,37 @@
+﻿using UnityEngine;
+
+namespace Gemini.Networking.Services {
+
+    [RequireComponent(typeof(Rigidbody))]
+    public class ForceController : MonoBehaviour
+    {
+
+        private Vector3 _force;
+        private Vector3 _torque;
+
+        private Rigidbody _rigidBody;
+
+        public Vector3 Force
+        {
+            set => _force = value;
+            get => _force;
+        }
+
+        public Vector3 Torque
+        {
+            set => _torque = value;
+            get => _torque;
+        }
+
+        void Start()
+        {
+            _rigidBody = gameObject.GetComponent<Rigidbody>();
+        }
+        void FixedUpdate()
+        {
+            _rigidBody.AddRelativeForce(_force);
+            _rigidBody.AddRelativeTorque(_torque);
+        }
+    }
+
+}
