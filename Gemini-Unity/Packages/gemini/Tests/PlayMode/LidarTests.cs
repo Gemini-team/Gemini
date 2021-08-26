0f793c212bd8202beca1fe50f4aa05ef228f9c7e
Thomas <thomas.skarshaug@zeabuz.com>
2021-08-26 15:57:43 +0200

    Change Lidar test scene and Playmode test

--- Gemini-Unity/Packages/gemini/Tests/PlayMode/LidarTests.cs
+++ Gemini-Unity/Packages/gemini/Tests/PlayMode/LidarTests.cs
@@ -3,12 +3,39 @@ using System.Collections.Generic;
 using NUnit.Framework;
 using UnityEngine;
 using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+using UnityEditor.SceneManagement;
+
+using Gemini.EMRS.Lidar;
 
 namespace Tests
 {
 
+
     public class LidarTest
     {
+
+       // public void Setup()
+       // {
+       //     //EditorSceneManager.OpenScene("Assets/Scenes/Test.unity");
+       //     //EditorSceneManager.OpenScene("C:\\Users\\ThomasSkarshaug\\Dev\\Simulation\\Gemini-team\\Gemini\\Gemini-Unity\\Packages\\gemini\\Runtime\\Gemini\\Examples");
+       // }
+
+
+        [OneTimeSetUp]
+        public void LoadScene()
+        {
+            SceneManager.LoadScene("Test");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            SceneManager.UnloadSceneAsync("Test");
+        }
+
+
+
         [UnityTest]
         public IEnumerator LidarMonoBehaviourTest()
         {
@@ -19,10 +46,40 @@ namespace Tests
 
     public class LidarMonoBehaviourTest : MonoBehaviour, IMonoBehaviourTest
     {
+
+        GameObject lidarObject;
+        private LidarScript lidar;       
         private int frameCount;
+        bool arrayNonZero = false;
+        //int numNonZeroBytes = 0;
         public bool IsTestFinished
         {
-            get { return frameCount  == 1 ; }
+            get 
+            { 
+                //Debug.Log("lidarArray length: " + lidar.lidarDataByte.array.Length);
+
+                //for (int i = 0; i < lidar.lidarDataByte.array.Length; i++)
+                //{
+                //    if (lidar.lidarDataByte.array[i] > 0.2)
+                //    {
+                //        numNonZeroBytes++;
+                //        arrayNonZero = true;
+                //    }
+                //}
+
+                //Debug.Log("nonZero bytes number: " + numNonZeroBytes);
+
+                Assert.AreEqual("test_frame_id", lidar.FrameId);
+
+                //Assert.AreEqual(true, arrayNonZero);
+
+                return frameCount == 1 ; 
+            }
+        }
+
+        void Awake()
+        {
+            lidar = FindObjectOfType<LidarScript>();
         }
 
         void Update()
