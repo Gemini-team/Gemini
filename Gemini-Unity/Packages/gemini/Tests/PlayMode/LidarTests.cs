20aebbf894525338d1faf7fee0bde3553f6616d9
Thomas <thomas.skarshaug@zeabuz.com>
2021-08-26 11:21:52 +0200

    Add PlayMode Test folde, assembly definition and dummy test script

---
+++ Gemini-Unity/Packages/gemini/Tests/PlayMode/LidarTests.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+
+namespace Tests
+{
+    public class LidarTest
+    {
+        // A UnityTest behaves like a coroutine in Play Mode. In Edit Mode you can use
+        // `yield return null;` to skip a frame.
+        [UnityTest]
+        public IEnumerator LidarTestWithEnumeratorPasses()
+        {
+            // Use the Assert class to test conditions.
+            // Use yield to skip a frame.
+            yield return null;
+        }
+    }
+}
