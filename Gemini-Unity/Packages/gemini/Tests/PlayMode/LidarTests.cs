5922dc97a6bc3557203b5ea0dde552f1d2e459e6
Thomas Skarshaug <thomas.skarshaug@zeabuz.com>
2021-08-27 10:45:46 +0200

    Feature/lidar tests (#70)
    * Add Test scene for PlayModeTests
    
    * Add PlayMode Test folde, assembly definition and dummy test script
    
    * Add dummy Monobehaviour test
    
    * Change Test scene
    
    * Move Test scene to Test/Scenes in Gemini package
    
    * Add Test scene to EditorBuildSettings
    
    * Delete Test scene from Examples folder
    
    * Comment out SendMessage, needed for test to work
    
    * Change Lidar test scene and Playmode test
    
    * Remove scene unload step
    
    * Remove debug log
    
    * Cleanup
    
    * Change TestMode for GH Actions to be EditMode only

---
+++ Gemini-Unity/Packages/gemini/Tests/PlayMode/LidarTests.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using NUnit.Framework;
+using UnityEngine;
+using UnityEngine.TestTools;
+using UnityEngine.SceneManagement;
+
+using Gemini.EMRS.Lidar;
+
+namespace Tests
+{
+
+    public class LidarTest
+    {
+
+
+
+        [OneTimeSetUp]
+        public void LoadScene()
+        {
+            SceneManager.LoadScene("Test");
+        }
+
+        [OneTimeTearDown]
+        public void TearDown()
+        {
+            SceneManager.UnloadSceneAsync("Test");
+        }
+
+        [UnityTest]
+        public IEnumerator LidarMonoBehaviourTest()
+        {
+            yield return new MonoBehaviourTest<LidarMonoBehaviourTest>();
+        }
+
+    }
+
+    public class LidarMonoBehaviourTest : MonoBehaviour, IMonoBehaviourTest
+    {
+
+        GameObject lidarObject;
+        private LidarScript lidar;       
+        private int frameCount;
+        bool arrayNonZero = false;
+        //int numNonZeroBytes = 0;
+        public bool IsTestFinished
+        {
+            get 
+            { 
+                Assert.AreEqual("test_frame_id", lidar.FrameId);
+
+                return frameCount == 1 ; 
+            }
+        }
+
+        void Awake()
+        {
+            lidar = FindObjectOfType<LidarScript>();
+        }
+
+        void Update()
+        {
+            frameCount++;
+        }
+    }
+}
