eee09a91943a8ee6c85670f9e64eee2221349aa2
Thomas <thomas.skarshaug@zeabuz.com>
2021-09-09 16:35:08 +0200

    Move Test util folder

---
+++ Gemini-Unity/Packages/gemini/Runtime/Gemini/Scripts/Misc/Util/SetupScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SetupScene
+{
+    public void SpawnSphere(float radius, Vector3 transform)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        // TODO: Scale by radius
+        sphere.transform = transform;
+    }
+}
