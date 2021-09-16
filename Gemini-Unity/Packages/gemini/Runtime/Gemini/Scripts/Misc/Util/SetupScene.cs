ce22e8ba893ba4d534971a3f51c8e407fe5e0e96
Thomas Skarshaug <thomas.skarshaug@zeabuz.com>
2021-09-16 10:52:59 +0200

    Feature/lidar tests (#74)
    * Move Test util folder
    
    * Add Sphere with flipped surface normals for testing
    
    * Lidar SphereTest WIP

---
+++ Gemini-Unity/Packages/gemini/Runtime/Gemini/Scripts/Misc/Util/SetupScene.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class SetupScene
+{
+    public static void SpawnSphere(float radius, Vector3 position)
+    {
+        GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+        sphere.transform.position = position;
+        sphere.transform.localScale = new Vector3(radius, radius, radius);
+    }
+}
