8ebb5363b73319109cb3e7a7c94b1bd6634bba7e
Mikael R Hanssen <mikaelrh@stud.ntnu.no>
2021-09-23 15:47:44 +0200

    Fix bounding box issues
    - Draw object-oriented bounding boxes (in screen space)
    - If object detection is enabled, automatically spawn a bounding box widget for each object tagged "Obstacle" (on start)
    - TODO: Spawn/remove bounding boxes when obstacles are instantiated/destroyed

---
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/UI/AIOverlay.cs
@@ -0,0 +1,31 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class AIOverlay : MonoBehaviour {
+    private const string OBSTACLE_TAG = "Obstacle";
+
+    public Sprite bbSprite;
+    public Color bbColor;
+
+    private void Start() {
+        if (PlayerPrefs.HasKey("ObjectDetection") && PlayerPrefs.GetInt("ObjectDetection") == 0) {
+            gameObject.SetActive(false);
+            return;
+        }
+
+        foreach (GameObject obstacle in GameObject.FindGameObjectsWithTag(OBSTACLE_TAG)) {
+            GameObject go = new GameObject();
+            go.transform.parent = transform;
+            
+            BoundingBoxWidget widget = go.AddComponent<BoundingBoxWidget>();
+            widget.target = obstacle;
+            
+            Image im = go.AddComponent<Image>();
+            im.sprite = bbSprite;
+            im.color = bbColor;
+            im.rectTransform.anchorMin = Vector2.zero;
+            im.rectTransform.anchorMax = Vector2.zero;
+            im.type = Image.Type.Sliced;
+        }
+    }
+}
---
+++ Gemini-Unity/Assets/Scripts/ManualTakeover/UI/Widgets/BoundingBoxWidget.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class BoundingBoxWidget : MonoBehaviour {
+    public GameObject target;
+    private Camera cam;
+
+    private Image image;
+    private RectTransform rt;
+
+    private IEnumerable<Vector3> BoundingVertices(Bounds bounds) {
+        for (int z = -1; z <= 1; z++) {
+            for (int y = -1; y <= 1; y++) {
+                for (int x = -1; x <= 1; x++) {
+                    if (x * y * z == 0) continue;
+
+                    Vector3 v = new Vector3(x, y, z);
+                    v.Scale(bounds.extents);
+
+                    yield return bounds.center + v;
+                }
+            }
+        }
+    }
+
+    private void Start() {
+        cam = Camera.main;
+        rt = GetComponent<RectTransform>();
+        image = GetComponent<Image>();
+    }
+
+    private void Update() {
+        Vector3 dir = target.transform.position - cam.transform.position;
+        if (Vector3.Dot(dir, cam.transform.forward) < 0) {
+            image.enabled = false;
+            return;  // Don't draw bounding box if object is behind camera
+        }
+        image.enabled = true;
+
+        Quaternion rot = target.transform.rotation;
+        target.transform.rotation = Quaternion.identity;
+
+        Bounds oobb = new Bounds(target.transform.position, Vector3.zero);
+        foreach (Renderer renderer in target.GetComponentsInChildren<Renderer>()) {
+            oobb.Encapsulate(renderer.bounds);
+        }
+
+        Bounds bounds = new Bounds(cam.WorldToScreenPoint(oobb.center), Vector3.zero);
+        foreach (Vector3 p in BoundingVertices(oobb)) {
+            bounds.Encapsulate(cam.WorldToScreenPoint(oobb.center + rot * (p - oobb.center)));
+        }
+
+        rt.anchoredPosition = bounds.center;
+        rt.sizeDelta = bounds.size;
+
+        target.transform.rotation = rot;
+    }
+}
